e37ba4daf9f106524966bbe0b637bbba133359a4
unknown <diptmang@DiptmanG.talentica-all.com>
2015-08-10 18:01:49 +0530

    testng updates

--- CommonCodePage/src/main/java/com/deep/common/Common.java
+++ CommonCodePage/src/main/java/com/deep/common/Common.java
@@ -3,7 +3,7 @@ package com.deep.common;
 import org.testng.annotations.Test;
 
 import com.deep.configuration.JavaBase;
-import com.deep.utility.ExcelUtils;
+
 
 @Test
 public class Common extends JavaBase {
@@ -13,16 +13,13 @@ public class Common extends JavaBase {
 	
 	
 
-	//	String username = CONFIGPROP.getProperty("username");
-		
-		String xusername = ExcelUtils.getCellData(1,1);
-		System.out.println(xusername);
+		String username = CONFIGPROP.getProperty("username");
+		System.out.println(username);
 		String password = CONFIGPROP.getProperty("password");
 		Thread.sleep(5000);
-		getObject("username_input").sendKeys(xusername);
+		getObject("username_input").sendKeys(username);
 		Thread.sleep(5000);
 		getObject("password_input").sendKeys(password);
-		
 		getObject("login_button").submit();
 	
 	}
---
+++ Suite/src/main/java/com/deep/testCase/LoginFb2.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.deep.testCase;
+
+import java.io.IOException;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.deep.common.Common;
+import com.deep.configuration.JavaBase;
+
+/**
+ * @author DiptmanG
+ *
+ */
+public class LoginFb2 extends JavaBase {
+
+	@BeforeTest
+	public void loadConfig() throws IOException {
+		Initialize();
+	}
+
+	@Test
+	public void loginmail() throws Exception {
+
+		driver.get(CONFIGPROP.getProperty("SiteName"));
+
+		Common common = new Common();
+		try {
+			common.login();
+
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
